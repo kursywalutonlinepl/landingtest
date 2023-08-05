672caa620cdf9afb8b11f6510bf867a059697cb4
kursywalutonlinepl <kursywalutonlinepl@gmail.com>
2023-08-05 13:46:22 +0300

    submit

--- components/Feature.js
+++ components/Feature.js
@@ -20,7 +20,7 @@ const Feature = () => {
     >
       <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8 p  y-8 my-12">
         <ScrollAnimationWrapper className="flex w-full justify-end">
-          <motion.div className="h-full w-1/2 p-4" variants={scrollAnimation}>
+          <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
             <Image
               src="/assets/Illustration3.png"
               alt="VPN Illustrasi"
@@ -61,6 +61,51 @@ const Feature = () => {
         </motion.div>
         </ScrollAnimationWrapper>
       </div>
+
+      <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8 p  y-8 my-12">
+        <ScrollAnimationWrapper className="flex w-full justify-end">
+          <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
+            <Image
+              src="/assets/Illustration3.png"
+              alt="VPN Illustrasi"
+              layout="responsive"
+              quality={100}
+              height={400}
+              width={400}
+            />
+          </motion.div>
+        </ScrollAnimationWrapper>
+        <ScrollAnimationWrapper>
+
+        <motion.div className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12" variants={scrollAnimation}>
+          <h3 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
+            We Provide Many Features You Can Use
+          </h3>
+          <p className="my-2 text-black-500">
+            Therapeutic experience
+          </p>
+          <ul className="text-black-500 self-start list-inside ml-8">
+            {features.map((feature, index) => (
+              <motion.li
+                className="relative circle-check custom-list"
+                custom={{duration: 2 + index}}
+                variants={scrollAnimation}
+                key={feature}
+                whileHover={{
+                scale : 1.1,
+                transition: {
+                  duration: .2
+                }
+                }}>
+                  {feature}
+              </motion.li>
+              )
+            )}
+          </ul>
+        </motion.div>
+        </ScrollAnimationWrapper>
+      </div>
+
     </div>
   );
 };
